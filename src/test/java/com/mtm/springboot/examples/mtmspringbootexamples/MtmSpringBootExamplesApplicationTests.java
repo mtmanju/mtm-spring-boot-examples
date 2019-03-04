5f2911aea1265154059a637e356f0163423b0936
Manjunath M T <ManjunathMT@fico.com>
2019-03-04 16:45:34 +0530

    initial commit

---
+++ src/test/java/com/mtm/springboot/examples/mtmspringbootexamples/MtmSpringBootExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mtm.springboot.examples.mtmspringbootexamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MtmSpringBootExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
